c2ef29b4211d13639459ccf5b6c00e2662761897
Manssizz <fajarmanssizz@gmail.com>
2020-12-03 14:38:23 +0700

    release folder after costumize rom

---
+++ release/flash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+UPDATEBIN="../tools/linux/update"
+$UPDATEBIN scan | grep 'No ' && exit 1
+
+echo "FLASHING"
+[ -e u-boot.bin ] && $UPDATEBIN partition bootloader u-boot.bin
+[ -e boot.img ] && $UPDATEBIN partition boot boot.img
+[ -e conf.img ] && $UPDATEBIN partition conf conf.img
+[ -e logo.img ] && $UPDATEBIN partition logo logo.img
+[ -e env.img ] && $UPDATEBIN partition env env.img
+$UPDATEBIN partition system system.img
+$UPDATEBIN bulkcmd "amlmmc erase data"
+$UPDATEBIN bulkcmd "amlmmc erase cache"
+$UPDATEBIN bulkcmd "setenv hdmimode 720p60hz"
+$UPDATEBIN bulkcmd "setenv -f EnableSelinux permissive"
+$UPDATEBIN bulkcmd "setenv firstboot 1"
+$UPDATEBIN bulkcmd "saveenv"
+echo "DONE"
+read -n 1
