2eef958aaa26f9b29fb0d3d16c0148617a54b8c8
manssizz <fajarmanssizz@gmail.com>
2020-12-23 10:27:29 +0700

    rename from app_installer to preinstall for general use v1, v2, dev

---
+++ master/v1/bin/preinstall.sh
@@ -0,0 +1,66 @@
+#!/system/bin/sh
+# Enable install FDisk
+settings put secure install_non_market_apps 1
+TOAST="am broadcast -a id.klampok.broadcast.CUSTOM_BROADCAST -e MSG "
+
+# Install custom APK
+find /system/preinstall/ -name "*\.apk" -exec sh -c '$1 "Installing $(basename $0 .apk)"; pm install $0' {} "$TOAST" \;
+
+$TOAST "Welcome to CendrawasihTV"
+$TOAST "Preparation for installing..."
+$TOAST "DON'T CLOSE OR POWEROFF YOUR DEVICE!!!"
+
+## Install Split APK
+#pm install-write -S
+
+# Data configuration
+cp -pr /system/data_default/* /data/
+
+# Enable Writable System Dir
+mount -o remount,rw /system
+#mount -o remount,rw /dev
+
+# grant some apps
+#pm grant com.goplay.sport android.permission.WRITE_EXTERNAL_STORAGE
+#pm grant com.goplay.sport android.permission.READ_EXTERNAL_STORAGE
+
+# Moving Data
+unzip -o /data/data.zip -d /data/data/ &> /dev/null
+unzip -o /data/su.zip -d /data/data/ &> /dev/null
+unzip -o /data/wifi.zip -d /data/misc/wifi/ &> /dev/null
+
+
+rm /data/data.zip
+rm /data/su.zip
+rm /data/wifi.zip
+
+
+rm -rf /system/data_default
+rm -rf /system/preinstall
+#rm /data/data1.zip
+
+$TOAST "Installing done, refreshing.."
+sleep 1
+
+$TOAST "PATCHING U-BOOT"
+dd if=/data/boot.img of=/dev/block/boot &> /dev/null
+#dd if=/data/bootloader.img of=/dev/block/bootloader &> /dev/null
+dd if=/data/u-boot.bin of=/dev/block/bootloader &> /dev/null
+rm /data/u-boot.bin
+rm /data/boot.img
+#rm /data/bootloader.img
+
+$TOAST "Cendrawasih TV"
+$TOAST "@Manssizz"
+$TOAST "Rebooting Device... Please Wait..."
+
+# Give some delay for launcer to receive broadcast
+sleep 10
+
+# Kill app to force reload modified data/config
+for f in /system/data_default/data/*/; do
+	killall $(basename $f)
+done
+
+sleep 1
+reboot
